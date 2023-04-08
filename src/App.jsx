bf360fa0d3c9e8cefbd2131156d5d51f67931453
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-08 10:04:50 +0200

    setting up movies table

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-
+import Movies from "./components/movies";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="container py-4 px-3 mx-auto">
-      <h1>Hello world</h1>
+      <Movies />
     </main>
   );
 }
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { getMovies } from "../services/fakeMovieService";
 
 function Movies() {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState(getMovies());
+
+  const handleDelete = (movie) => {
+    const newMovies = [...movies];
+    newMovies.filter((newMovie) => newMovie._id !== movie);
+    return setMovies(newMovies);
+  };
+
+  return (
+    <div>
+      <p>Showing {movies.length} movies in the database</p>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Genre</th>
+            <th>Stock</th>
+            <th>Rate</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          {movies.map((movie) => (
+            <tr key={movie._id}>
+              <td>{movie.title}</td>
+              <td>{movie.genre.name}</td>
+              <td>{movie.numberInStock}</td>
+              <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(movie._id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Movies;
