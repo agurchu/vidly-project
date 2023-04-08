8b1dfe6de7afa34c7640362474915528f9441ddb
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-08 10:18:12 +0200

    handledelete function

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,41 +6,46 @@ function Movies() {
 
   const handleDelete = (movie) => {
     const newMovies = [...movies];
-    newMovies.filter((newMovie) => newMovie._id !== movie);
-    return setMovies(newMovies);
+    return setMovies(newMovies.filter((newMovie) => newMovie._id !== movie));
   };
 
   return (
     <div>
-      <p>Showing {movies.length} movies in the database</p>
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-          </tr>
-        </thead>
-        <tbody className="table-group-divider">
-          {movies.map((movie) => (
-            <tr key={movie._id}>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(movie._id)}
-                >
-                  Delete
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {movies.length === 0 ? (
+        <p>There are no movies in the database</p>
+      ) : (
+        <div>
+          <p>Showing {movies.length} movies in the database</p>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              {movies.map((movie) => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(movie._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
