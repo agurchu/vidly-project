0ce1d77a25b1e9b5f708632ccd9324e8baaa655d
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-29 10:52:06 +0200

    Route Parameters

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from "react";
 import Movies from "./components/movies";
 import "./App.css";
 import NavBar from "./components/navBar";
-import { Route, Routes, Redirect } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/home";
 import Products from "./components/products";
 import Posts from "./components/posts";
 import Admin from "./components/admin";
 import NotFound from "./components/notFound";
+import ProductDetails from "./components/productDetails";
 
 function App() {
   return (
@@ -17,12 +18,13 @@ function App() {
       <main className="container py-4 px-3 mx-auto">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/products" element={<Products />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/posts/:year?/:month?" element={<Posts />} />
           <Route path="/movies" element={<Movies />} />
-          <Route path="/not-found" element={<NotFound />} />
-          <Redirect to="/not-found" />
+          {/* <Route path="/not-found" element={<NotFound />} />
+          <Redirect to="/not-found" /> */}
         </Routes>
       </main>
     </>
